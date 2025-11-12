edcf220ad05b92b24f048c1f5eea48eb4ed1bd6d
Hamza Labbaalli <hamzalabbaalli@gmail.com>
2025-11-12 23:39:26 +0100

    Fixing Server cors by hlnajz

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ require("dotenv").config();
 require("./db/conn");
 const path = require("path");
 
+const app = express(); // create app first
+
+// fix CORS placement
+app.use(cors({
+  origin: ["https://uizhospital.com"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+app.options("*", cors());
+
 // Routers
 const userRouter = require("./routes/userRoutes");
 const doctorRouter = require("./routes/doctorRoutes");
@@ -11,11 +22,9 @@ const appointRouter = require("./routes/appointRoutes");
 const notificationRouter = require("./routes/notificationRouter");
 const assuranceRoutes = require("./routes/assuranceRoutes");
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,16 +35,14 @@ app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
 app.use("/api/rpc", assuranceRoutes);
 
-// Serve React build in production
+// optional: React build serving
 // app.use(express.static(path.join(__dirname, "./client/build")));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
-// Log secret for debug (remove in production)
-console.log("FORMIK SECRET:", process.env.REACT_APP_FORMIK_SECRET);
 
-// Start server
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
